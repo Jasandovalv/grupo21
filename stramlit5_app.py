a31d2928618e8fcbcb340cf4d10e27125526af7c
jasandovalv <jasandovalv@udd.cl>
2025-11-21 17:58:59 -0300

    first commit

---
+++ stramlit5_app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+# --------------------------------------
+# Configuración de la página
+# --------------------------------------
+st.set_page_config(page_title="Gráfico de emisiones CO₂", layout="wide")
+st.title("📊 Gráfico de barras – Emisiones de CO₂ por país")
