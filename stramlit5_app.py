3d8502884a6db64e355689d29f5212906ae36ccc
Jasandovalv <jaisandovalv@udd.cl>
2025-11-21 18:04:16 -0300

    Update title and add data loading section

--- stramlit5_app.py
+++ stramlit5_app.py
@@ -5,3 +5,7 @@ import streamlit as st
 # --------------------------------------
 st.set_page_config(page_title="Gráfico de emisiones CO₂", layout="wide")
 st.title("📊 Gráfico de barras – Emisiones de CO₂ por país")
+# --------------------------------------
+# Cargar datos
+# --------------------------------------
+csv_path = "/Users/jaimesandoval/Desktop/grupo21/co2/emissions_per_country/annual-co2-emissions-per-country.csv"
